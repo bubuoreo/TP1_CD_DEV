31d413362033a3e06e8f8c7e6cbb071e753037e3
Alexandre Burlot <alexandre@Macbook-air-de-Alexandre.local>
2020-11-24 08:42:55 +0100

    TODO list

--- Calendar.py
+++ Calendar.py
@@ -1,3 +1,11 @@
-'''
-Burlot Alexandre
-24/11/2020
+# Burlot Alexandre
+# 24/11/2020
+# TODO: 
+# - Écrire une fonction qui teste si une année est bissextile ou non avec la convention suivante : la fonction
+# renvoie la valeur True si l'année est bissextile, la valeur False dans le cas contraire.
+# - Écrire une fonction qui donne le nombre de jours d'un mois après avoir vérifié la validité des
+# paramètres.
+# - Écrire une fonction qui vérifiera si une date est valide ou pas (True signifie date valide, False sinon)
+# - Écrire le programme principal qui propose la saisie d'une date, la valide et affiche le message "date
+# valide" ou "date non valide" selon le cas
+
